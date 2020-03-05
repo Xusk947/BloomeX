7aaefeede5b0a672e5188d77d4413ed47580e70a
Xusk947 <56998647+Xusk947@users.noreply.github.com>
2020-03-05 02:00:53 +0000

    Delete nuke-car.js

--- scripts/nuke-car.js
+++
@@ -1,8 +0,0 @@
-const nukeCar = extendContent(unitType, "nuke-car", {
-    onDeath(){
-        Damage.damage(tile.worldx(), tile.worldy(), explosionRadius * 30 *tilesize, explosionDamage * 30)
-
-    }
-});
-
-nukeCar.localizedName :"Nuke Car";
