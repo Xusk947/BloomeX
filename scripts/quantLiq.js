3832a67a6a66ebc396e0898ff73605ad74cd6ad2
Summet <55251897+Summet-V@users.noreply.github.com>
2020-04-28 22:29:33 +0300

    Update quantLiq.js
    Some code-style changes.

--- scripts/quantLiq.js
+++ scripts/quantLiq.js
@@ -1,27 +1,18 @@
-
-
 const quantFact = extendContent(GenericCrafter, "quantLiq", {
-
-draw(tile){
-    Draw.rect(this.region, tile.drawx(), tile.drawy());
-    },
     
-generateIcons: function(){
-		return [
-			Core.atlas.find(this.name),
-		];
-	},
-	
-update(tile){
-	
-	},
-
-})
+    draw(tile){
+        Draw.rect(this.region, tile.drawx(), tile.drawy());
+    },
+        
+    generateIcons() {
+        return [
+            Core.atlas.find(this.name),
+        ];
+    },
+    	
+    update(tile) {}
+});
 
 quantFact.configurable = true;
 quantFact.consumes.item();
 quantFact.outputItem = new ItemStack(Items.lead, 1);
-
-
-
-
