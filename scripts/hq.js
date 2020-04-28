735de36d7ec1c75bca4a87c81c7988e861f6f4a6
Summet <55251897+Summet-V@users.noreply.github.com>
2020-04-28 22:17:04 +0300

    Update hq.js
    Some code-style changes.

--- scripts/hq.js
+++ scripts/hq.js
@@ -1,132 +1,105 @@
-
-var lvl = 1;
-var time = 600
-var i = time // Mathf.random(3,5) * 60 * 60;
-var tru = 0;
+let lvl = 1;
+let time = 600
+let i = time // Mathf.random(3,5) * 60 * 60;
+let tru = 0;
 
 
 Events.on(EventType.PlayEvent, cons(e => {
     lvl = 1;
 }));
 
-
-
-
-
-
 const hq = extendContent(CoreBlock , "hq", {
-
-draw(tile){
+	
+draw(tile) {
     Draw.rect(this.region, tile.drawx(), tile.drawy());
-    },
+},
     
 generateIcons: function(){
-		return [
-			Core.atlas.find(this.name),
-		];
-	},
+    return [
+        Core.atlas.find(this.name),
+    ];
+},
 	
 	
 	
-update(tile){
-    
+update(tile) {
     if (tile.entity.timer.get(0, i)) {
-        
         switch (lvl) {
+            case 1: 
+                UnitTypes.wraith.create(Team.sharded).add();
+                lvl++;
             
-        case 1: 
-            UnitTypes.wraith.create(Team.sharded).add();
-            lvl++;
-            
-            time = time * 1;
-            break;
-            
-        
-        case 2:
-            lvl++;
-            time = time * 2;
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            break;
-            
-        case 3:
-            lvl++;
-            time = time * 1.5;
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            break;
-            
-        case 4:
-            lvl++;
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            break;
-            
-        case 5:
-            lvl++;
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.revenant.create(Team.sharded).add();
-            break;
-            
-            
-        case 6:
-            lvl++;
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.revenant.create(Team.sharded).add();
-            break;
-            
+                time = time * 1;
+                break;
+            case 2:
+                lvl++;
+                time = time * 2;
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                break; 
+            case 3:
+                lvl++;
+                time = time * 1.5;
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                break;
+            case 4:
+                lvl++;
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                break;
+            case 5:
+                lvl++;
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.revenant.create(Team.sharded).add();
+                break;
+            case 6:
+                lvl++;
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.revenant.create(Team.sharded).add();
+                break;
             case 7:
-            lvl++;
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.revenant.create(Team.sharded).add();
-            UnitTypes.revenant.create(Team.sharded).add();
-            break;
+                lvl++;
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.revenant.create(Team.sharded).add();
+                UnitTypes.revenant.create(Team.sharded).add();
+                break;
             case 8:
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.wraith.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.ghoul.create(Team.sharded).add();
-            UnitTypes.revenant.create(Team.sharded).add();
-            UnitTypes.revenant.create(Team.sharded).add();
-            break;
-
-        }
-
-        };
-        
-    
-	},
-drawLayer(tile){
-
-    Draw.rect(Core.atlas.find(this.name + "-radar"), tile.drawx() - 8.3, tile.drawy() + 8.4,2 * Time.time()); 
-	},
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.wraith.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.ghoul.create(Team.sharded).add();
+                UnitTypes.revenant.create(Team.sharded).add();
+                UnitTypes.revenant.create(Team.sharded).add();
+                break;
+        }};
+    },
 	
-	
-
-
-
+    drawLayer(tile){
+        Draw.rect(Core.atlas.find(this.name + "-radar"), tile.drawx() - 8.3, tile.drawy() + 8.4,2 * Time.time());
+    },
 })
  
  
-//hq.mech = Alpha;
+// hq.mech = Alpha;
