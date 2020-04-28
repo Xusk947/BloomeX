b8c6b9d941c5eabeef7aa19a5474a076221102fd
Summet <55251897+Summet-V@users.noreply.github.com>
2020-04-28 22:20:40 +0300

    Update bomb.js
    Some code-style changes.

--- scripts/bomb.js
+++ scripts/bomb.js
@@ -1,38 +1,30 @@
-
-
-
-const chilster = extendContent(UnitType  , "chilster",{
-    
-    
-})
-
+const chilster = extendContent(UnitType, "chilster", {})ж
+ 
 chilster.create(prov(() => new JavaAdapter(FlyingUnit, { 
-    draw(){
+    draw() {
     	const vec = new Vec2();
         const rotation = Time.time() * 25;
         const reg = Core.atlas.find("bloomex-helicopter-rotator");
         
-        for ( var s = 0; s < 2 ; s++){
-        var	signB = [1 , -1];
-        var	sign = signB[s];
-        vec.trns(this.rotation , 2.75 , sign *16);
-        Draw.rect(reg , this.x + vec.x, this.y + vec.y, reg.getWidth() * sign * Draw.scl, reg.getHeight() * Draw.scl,sign * rotation  );
-        Draw.mixcol(Color.blue, this.hitTime / this.hitDuration);
-        Draw.mixcol();
-     //   Draw.shader(Shadow , shadow);
-        this.drawLight();
-      };
-//  Draw.color();
-    this.super$draw();
-
-      
+        for (var s = 0; s < 2; s++) {
+            var	signB = [1, -1];
+            var	sign = signB[s];
         
-           
+            vec.trns(this.rotation , 2.75 , sign *16);
+             Draw.rect(reg , this.x + vec.x, this.y + vec.y, reg.getWidth() * sign * Draw.scl, reg.getHeight() * Draw.scl,sign * rotation  );
+            Draw.mixcol(Color.blue, this.hitTime / this.hitDuration);
+            Draw.mixcol();
+            
+            // Draw.shader(Shadow , shadow);
+            this.drawLight();
+        };
         
-    }, 
-    update(){
-this.super$update();
-}
-    })));
+        // Draw.color();
+        this.super$draw();
+    },
+    
+    update() {
+        this.super$update();
+    }})));
     
 //helicopter = this.global.helicopter ;
