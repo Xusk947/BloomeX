badb0336a0db8d5c31c1eeeaffce8c34f8a96cf2
Xusk947 <56998647+Xusk947@users.noreply.github.com>
2020-04-29 04:06:15 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,12 @@
 require("heli");
 require("hq");
 require("heal-point");
-<<<<<<< HEAD
+
 require("omni-drill");
 require("biolint-factory");
 require("biolint");
 print("load");
-=======
->>>>>>> 2f015a33bed111e1678729d4ed3b7fbe00913e51
-//require("advanced-separator");
 
-//require("bomb");
+
+
+
